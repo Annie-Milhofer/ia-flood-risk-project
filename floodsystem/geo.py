e68f03f46c87d76072ab6ef9fb5ea75bd252a62e
Annie Milhofer <azm24@cam.ac.uk>
2022-02-02 20:01:01 +0000

    started 1D

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,7 +6,7 @@ geographical data.
 
 """
 #from .stationdata import build_station_list  
-# ((stations(2,0)-p(0))**2 + (stations(2,1)-p(1))**2)**1/2
+# attempted alternative to haversine... ((stations(2,0)-p(0))**2 + (stations(2,1)-p(1))**2)**1/2
 #haversine(stations.coord,p)
 #from dis import _HaveCodeOrStringType
 from .utils import sorted_by_key  # noqa
@@ -35,3 +35,19 @@ def stations_within_radius(stations, centre, r):
             continue
     return stations_in
 
+# Task 1D
+def rivers_with_station(stations):
+    river_list = []
+    all_rivers = [stations[3]]
+    for i in all_rivers:
+        if all_rivers[i] in river_list:
+            continue
+        else:
+            river_list.append(all_rivers[i])
+    return river_list
+
+def stations_by_river(stations):
+    river_dict = {}
+    for i in stations:
+        if stations.river not in river_dict:
+            
