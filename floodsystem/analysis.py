c56c884474d5ff7792b5d3357397d5067f430e34
B1inkFox <61612889+B1inkFox@users.noreply.github.com>
2022-02-24 03:03:35 +0000

    Update analysis.py
    Updated comments

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -2,8 +2,7 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
-
-
+#Task 2F
 def polyfit(dates, levels, p):
     dates_num = matplotlib.dates.date2num(dates)
     p_coeff = np.polyfit(dates_num - dates_num[0], levels, p)
