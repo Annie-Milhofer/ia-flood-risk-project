a7b6de3a1fbb1265aa8726a6e853ca1b2cb2edd4
B1inkFox <61612889+B1inkFox@users.noreply.github.com>
2022-02-24 03:24:36 +0000

    Deleted unused import

--- Task2C.py
+++ Task2C.py
@@ -1,6 +1,6 @@
 from floodsystem.stationdata import build_station_list, update_water_levels
-from floodsystem.station import MonitoringStation 
 from floodsystem.flood import stations_highest_rel_level
+
 def run():
     """Requirements for Task 2C"""
     # Build list of stations
