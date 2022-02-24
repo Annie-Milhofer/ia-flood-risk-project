f0287a3207bfd6ee30a6fd70a80f7173c02f6d0b
B1inkFox <61612889+B1inkFox@users.noreply.github.com>
2022-02-24 03:14:01 +0000

    Update Task2F.py
    Added comments

--- Task2F.py
+++ Task2F.py
@@ -13,7 +13,9 @@ def run():
     for station_list in high_risk_stations_list:
         station = station_list[0]
         dates, levels = fetch_measure_levels(station.measure_id, dt=timedelta(days=2))
+        #plot water level without typical range
         plot_water_level_with_fit(station, dates, levels, 4)
+        #plot water level with typical range
         plot_water_level_with_fit(station, dates, levels, 4, show_high_low_range=True, dot_like_original_data=False)
 
 if __name__ == "__main__":
